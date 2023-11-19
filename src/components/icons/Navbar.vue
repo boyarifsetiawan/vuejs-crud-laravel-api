5e6f2db9c67bece5ca264ff7325a1163efcb3829
boyarifsetiawan <boysetiawan5635@gmail.com>
2023-11-19 21:16:46 +0700

    Membuat Navbar

---
+++ src/components/icons/Navbar.vue
@@ -0,0 +1,28 @@
+<template>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container">
+    <RouterLink class="navbar-brand" to="/">Navbar</RouterLink>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <RouterLink class="navbar-brand" to="/">Home</RouterLink>
+        </li>
+        <li class="nav-item">
+          <RouterLink class="navbar-brand" to="/about">About</RouterLink>
+        </li>
+        <li class="nav-item">
+          <RouterLink class="navbar-brand" to="/students">Students</RouterLink>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</template>
+
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Students/View.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Student pages</h1>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
