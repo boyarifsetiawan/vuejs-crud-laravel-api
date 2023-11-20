4510e108ba0b29a9e81a2a611925f9540935eeb2
boyarifsetiawan <boysetiawan5635@gmail.com>
2023-11-20 20:40:15 +0700

    Add getStudents with axios in view students adn create view student

---
+++ src/views/Students/Create.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="container mt-5">
+    <div class="card">
+      <div class="card-header"> 
+        <h4>Create Student</h4>
+      </div>
+      <div class="card-body">
+        <div class="mb-3 row">
+          <label class="col-sm-2 col-form-label">Name</label>
+          <div class="col-sm-10">
+            <input type="text" class="form-control">
+          </div>
+        </div>
+        <div class="mb-3 row">
+          <label class="col-sm-2 col-form-label">Course</label>
+          <div class="col-sm-10">
+            <input type="text" class="form-control">
+          </div>
+        </div>
+        <div class="mb-3 row">
+          <label class="col-sm-2 col-form-label">Email</label>
+          <div class="col-sm-10">
+            <input type="email" class="form-control">
+          </div>
+        </div>
+        <div class="mb-3 row">
+          <label class="col-sm-2 col-form-label">Phone</label>
+          <div class="col-sm-10">
+            <input type="text" class="form-control">
+          </div>
+        </div>
+        <hr>
+        <div class="mb-5">
+          <button class="btn btn-success float-start">Add</button>
+          <RouterLink to="/students" class="btn btn-danger float-start mx-4">Cancel</RouterLink>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
--- src/views/Students/View.vue
+++ src/views/Students/View.vue
@@ -1,11 +1,69 @@
 <template>
-  <h1>Student pages</h1>
+  <div class="card">
+    <div class="card-header"> 
+      <h4>Students 
+        <RouterLink to="/students/create" class="btn btn-primary float-end">Add Student</RouterLink>
+      </h4>
+    </div>
+    <div class="card-body">
+      <table class="table table-bordered">
+        <thead>
+          <tr>
+            <th>NO</th>
+            <th>Name</th>
+            <th>Course</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody v-if="this.students.length > 0">
+          <tr v-for="(student, index) in this.students" :key="index">
+            <td>{{ ++index }}</td>
+            <td>{{ student.name }}</td>
+            <td>{{ student.course }}</td>
+            <td>{{ student.email }}</td>
+            <td>{{ student.phone }}</td>
+            <td>
+              <RouterLink to="#" class="btn btn-warning mx-3">Edit</RouterLink>
+              <button class="btn btn-danger">Delete</button>
+            </td>
+          </tr>
+        </tbody>
+        <tbody v-else>
+          <tr>
+            <td colspan="5">Loading...</td>
+          </tr>
+        </tbody>
+
+      </table>
+    </div>
+
+  </div>
 </template>
 
 <script>
+import axios from 'axios';
+import { RouterLink } from 'vue-router';
+export default{
+  name:'students',
+  data(){
+    return {
+      students:''
+    }
+  },
+  mounted(){
+    this.getStudents();
+  },
+  methods:{
 
+    getStudents(){
+
+      axios.get('http://127.0.0.1:8000/api/students').then( res => {
+        this.students = res.data.students
+        console.log(res.data.students)
+      });
+    }
+  }
+}
 </script>
-
-<style>
-
-</style>
