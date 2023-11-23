bc6c0ea59b1414cb5529393e8ff75aabc5a2ba13
boyarifsetiawan <boysetiawan5635@gmail.com>
2023-11-23 20:41:39 +0700

    Add delete function

--- src/views/Students/View.vue
+++ src/views/Students/View.vue
@@ -26,13 +26,20 @@
             <td>{{ student.phone }}</td>
             <td>
               <RouterLink :to="{path: '/students/'+student.id+'/edit'}" class="btn btn-warning mx-3">Edit</RouterLink>
-              <button class="btn btn-danger">Delete</button>
+              <button class="btn btn-danger" @click="deleteStudent(student.id)">Delete</button>
+            </td>
+          </tr>
+        </tbody>
+        <tbody v-else-if="this.data.length > 0">
+          <tr>
+            <td colspan="6">Student Not Found <br>
+              <RouterLink to="/students/create" class="btn btn-primary mt-3">Create Student Now</RouterLink>
             </td>
           </tr>
         </tbody>
         <tbody v-else>
           <tr>
-            <td colspan="5">Loading...</td>
+            <td colspan="5">Loading....</td>
           </tr>
         </tbody>
 
@@ -44,12 +51,13 @@
 
 <script>
 import axios from 'axios';
-import { RouterLink } from 'vue-router';
+// import router from '../../router'
 export default{
   name:'students',
   data(){
     return {
-      students:''
+      students:'',
+      data:''
     }
   },
   mounted(){
@@ -58,11 +66,22 @@ export default{
   methods:{
 
     getStudents(){
-
+      var my = this
       axios.get('http://127.0.0.1:8000/api/students').then( res => {
         this.students = res.data.students
         console.log(res.data.students)
-      });
+      }).catch(function (error){
+        my.data = error.response.data.message
+        alert(error.response.data.message)
+        console.log(error.response.data.message)
+      })
+    },
+    deleteStudent(id){
+      axios.delete(`http://127.0.0.1:8000/api/students/${id}/delete`).then( res => {
+        confirm('Are you sure ?')
+        alert(res.data.message)
+        console.log(res.data.message)
+      })
     }
   }
 }
